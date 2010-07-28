3c75fd68fc34f8c68a95a225b890a5d539b9c1e2
Pinifloyd <hanchar.sergey@gmail.com>
2010-07-28 20:19:50 +0300

    Сделал таск Б из книги

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,10 @@ class Product < ActiveRecord::Base
 											:with => %r{\.(gif|jpg|png)$}i,
 											:message => 'Must be finish at gif/png/jpg'
 
+	def self.find_products_for_sale
+		find(:all, :order => "title")
+	end
+
 protected
 	def price_must_be_at_least_a_cent
 		errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
