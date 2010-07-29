5a474e931587c0c19811e876cc1f0f74917fc35c
Pinifloyd <hanchar.sergey@gmail.com>
2010-07-29 13:35:09 +0300

    добавил paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,13 @@
 class Product < ActiveRecord::Base
-  attr_accessible :title, :description, :image_url, :price
+  attr_accessible :title, :description, :price, :picture
 	
-	validates_presence_of :title, :description, :image_url
+	validates_presence_of :title, :description
 	validates_length_of :title, :maximum => 10, :message => "has more than 10 simbols"
 	validates_numericality_of :price
 	validate :price_must_be_at_least_a_cent
 	validates_uniqueness_of :title
-	validates_format_of :image_url,
-											:with => %r{\.(gif|jpg|png)$}i,
-											:message => 'Must be finish at gif/png/jpg'
+
+	has_attached_file :picture, :styles => { :small => "100x100>" }
 
 	def self.find_products_for_sale
 		find(:all, :order => "title")
