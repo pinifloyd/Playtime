ac815c804c9a75d0e518555af68e2f86c3b8b449
Pinifloyd <hanchar.sergey@gmail.com>
2010-08-05 17:17:56 +0300

    коментарии создаются. сделал вывод коментариев.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :product
 	
-	acts_as_nested_set
-  def path_name
-    "#{'<ol>' * self.level} #{self.name}"
-  end
+	acts_as_nested_set :scope => :product
 end
